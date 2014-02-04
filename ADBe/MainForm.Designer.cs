62e5e640b2e90dee17897729228a6d2342b5f54f
A7maDev <A7maDev@Gmail.com>
2014-02-04 16:32:50 +0300

    Init Project

---
+++ ADBe/MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ADBe
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.enableButton = new System.Windows.Forms.Button();
+            this.disableButton = new System.Windows.Forms.Button();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Image = global::ADBe.Properties.Resources.adbe_enabled;
+            this.logoPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(346, 223);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.logoPictureBox.TabIndex = 0;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // enableButton
+            // 
+            this.enableButton.ForeColor = System.Drawing.Color.DarkGreen;
+            this.enableButton.Location = new System.Drawing.Point(212, 251);
+            this.enableButton.Name = "enableButton";
+            this.enableButton.Size = new System.Drawing.Size(130, 23);
+            this.enableButton.TabIndex = 1;
+            this.enableButton.Text = "Enable ADB";
+            this.enableButton.UseVisualStyleBackColor = true;
+            // 
+            // disableButton
+            // 
+            this.disableButton.ForeColor = System.Drawing.Color.Red;
+            this.disableButton.Location = new System.Drawing.Point(212, 290);
+            this.disableButton.Name = "disableButton";
+            this.disableButton.Size = new System.Drawing.Size(130, 23);
+            this.disableButton.TabIndex = 2;
+            this.disableButton.Text = "Disable ADB";
+            this.disableButton.UseVisualStyleBackColor = true;
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(26, 254);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(160, 20);
+            this.ipTextBox.TabIndex = 3;
+            this.ipTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(60, 290);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(100, 20);
+            this.portTextBox.TabIndex = 4;
+            this.portTextBox.Text = "5555";
+            this.portTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 338);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.ipTextBox);
+            this.Controls.Add(this.disableButton);
+            this.Controls.Add(this.enableButton);
+            this.Controls.Add(this.logoPictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ADBe";
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Button enableButton;
+        private System.Windows.Forms.Button disableButton;
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.TextBox portTextBox;
+
+    }
+}
+
